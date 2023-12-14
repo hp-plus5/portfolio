bf4253af1213a72b532a65b40270e28575fdddc1
Sam Warner <its.sam.warner@gmail.com>
2023-12-14 09:46:02 -0500

    Chaotic merge to salvage code from a dying laptop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,7 @@
-// Scroll to top
+
+/**
+ * Scroll to top button behavior
+ */
 const scrollUp = document.querySelector("#scroll-up");
 
 scrollUp.addEventListener("click", () => {
@@ -8,3 +11,49 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+/**
+ * Selector helper function
+ */
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
+  }
+}
+
+/**
+ * Hero type effect
+ */
+const typed = select('.typed')
+if (typed) {
+  let typed_strings = typed.getAttribute('data-typed-items')
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
+
+
+/**
+ * Skills animation
+ */
+let skillsContent = select('.skills-content');
+if (skillsContent) {
+  new Waypoint({
+    element: skillsContent,
+    offset: '80%',
+    handler: function(direction) {
+      let progress = select('.progress .progress-bar', true);
+      progress.forEach((el) => {
+        el.style.width = el.getAttribute('aria-valuenow') + '%'
+      });
+    }
+  })
+}
